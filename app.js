0d58611fa369f112d13d82963580c092382b5759
Paul Maxineanu <paul.maxineanu@gmail.com>
2021-01-19 18:32:21 +0200

    check question answers and display quiz result

--- app.js
+++ app.js
@@ -10,19 +10,37 @@ class Quiz {
         this.questionsPoints = document.querySelector('.questions-info');
         this.quiz = '';
         this.current_question = 0;
+        this.current_question_index = 0;
         this.nextQuestion = 0;
-
-        //events
+        this.final_score = 0;
 
     }
 
+    resetSelectors() {
+        this.answers = document.querySelector('.answers');
+        this.allAnswers = document.querySelectorAll('.answer');
+        this.nextBtn.disabled = true;
+    }
+
+    checkAnswer() {
+        let activeAnswer = document.querySelector('.answer.active');
+        let activeAnsweIndex = activeAnswer.getAttribute('data-index');
+        if (this.quiz.questions[this.current_question_index].answers[activeAnsweIndex].isCorrect === true) {
+            this.final_score++;
+        }
+    }
+
     loadNextQuestion(e) {
-        console.log(this.nextQuestion, this.quiz.questions.length)
         if (this.nextQuestion < this.quiz.questions.length) {
+            this.checkAnswer();
             this.getQuestion(this.nextQuestion);
         } else {
-            alert("Quiz finished, display results now");
+            //check final answer and then reset quiz
+            this.checkAnswer();
+            alert("Yo've finished the quiz, you guessed " + this.final_score + " of " + this.quiz.questions.length);
+
         }
+        this.current_question_index++;
     }
 
     async fetchQuiz(url) {
@@ -45,12 +63,14 @@ class Quiz {
         this.current_question++;
         this.question_nr.innerText = this.current_question;
         this.question_title.innerText = this.quiz.questions[key].title;
-        this.quiz.questions[key].answers.forEach(answer => {
+        this.quiz.questions[key].answers.forEach((answer, index) => {
             let new_answer = document.createElement('li');
             new_answer.classList.add('answer');
             new_answer.innerText = answer.answer;
+            new_answer.setAttribute('data-index', index);
             this.answers.appendChild(new_answer);
         });
+        this.addEventsToAnswers();
         this.nextQuestion++;
     }
 
@@ -63,15 +83,30 @@ class Quiz {
         });
     }
 
-    setActiveAnswer(event) {
-        console.log(event);
+
+
+    addEventsToAnswers() {
+        this.resetSelectors();
+        this.allAnswers.forEach(answer => {
+            answer.addEventListener('click', function (event) {
+                event.target.classList.toggle('active');
+                if (event.target.classList.contains('active')) {
+                    document.querySelector('.quiz-nav').disabled = false;
+                } else {
+                    document.querySelector('.quiz-nav').disabled = true;
+                }
+                event.target.parentNode.childNodes.forEach(other => {
+                    if (event.target != other) {
+                        other.classList.remove('active');
+                    }
+                });
+            })
+        });
     }
 
     async getQuiz(url) {
         const data = await this.fetchQuiz(url);
         this.quiz = data.quiz;
-        console.log(quiz);
-
         this.getQuestion(0);
 
         this.getQuestionsPoints();
@@ -92,6 +127,9 @@ quiz.getQuiz('./quiz.json');
 
 quiz.nextBtn.addEventListener('click', function () {
     quiz.loadNextQuestion();
+    if (quiz.current_question == 10) {
+        quiz.nextBtn.innerText = 'finish quiz';
+    }
 });
 
 
